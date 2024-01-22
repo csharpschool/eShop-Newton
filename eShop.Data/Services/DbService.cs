6d8c605ca1bd25c899ba281d9ba85abf09b2b391
Jonas Fagerberg <info@csharpschool.com>
2024-01-22 12:05:24 +0100

    Extension/EF Category Klar

--- eShop.Data/Services/DbService.cs
+++ eShop.Data/Services/DbService.cs
@@ -23,4 +23,44 @@ public class DbService : IDbService
         var entities = await _db.Set<TEntity>().ToListAsync();
         return _mapper.Map<List<TDto>>(entities);
     }
+
+    public virtual async Task<TDto> SingleAsync<TEntity, TDto>(int id) where TEntity : class, IEntity where TDto : class
+    {
+        var entity = await _db.Set<TEntity>().SingleOrDefaultAsync(e => e.Id == id);
+        return _mapper.Map<TDto>(entity);
+    }
+    public async Task<TEntity> AddAsync<TEntity, TDto>(TDto dto) where TEntity : class where TDto : class
+    {
+        var entity = _mapper.Map<TEntity>(dto);
+        await _db.Set<TEntity>().AddAsync(entity);
+        return entity;
+    }
+
+    public async Task<bool> SaveChangesAsync() => await _db.SaveChangesAsync() >= 0;
+
+    public void Update<TEntity, TDto>(TDto dto)
+    where TEntity : class, IEntity where TDto : class
+    {
+        // Note that this method isn't asynchronous because Update modifies
+        // an already exisiting object in memory, which is very fast.
+        var entity = _mapper.Map<TEntity>(dto);
+        _db.Set<TEntity>().Update(entity);
+    }
+
+    public async Task<bool> DeleteAsync<TEntity>(int id)
+        where TEntity : class, IEntity
+    {
+        try
+        {
+            var entity = await _db.Set<TEntity>()
+                .SingleOrDefaultAsync(e => e.Id == id);
+
+            if (entity is null) return false;
+            _db.Remove(entity);
+        }
+        catch { return false; }
+
+        return true;
+    }
+
 }
--- eShop.Data/Services/IDbService.cs
+++ eShop.Data/Services/IDbService.cs
@@ -6,4 +6,19 @@ public interface IDbService
         where TEntity : class
         where TDto : class;
 
+    Task<TDto> SingleAsync<TEntity, TDto>(int id)
+        where TEntity : class, IEntity where
+        TDto : class;
+
+    Task<TEntity> AddAsync<TEntity, TDto>(TDto dto)
+        where TEntity : class where TDto : class;
+
+    void Update<TEntity, TDto>(TDto dto)
+        where TEntity : class, IEntity
+        where TDto : class;
+
+    Task<bool> DeleteAsync<TEntity>(int id) 
+        where TEntity : class, IEntity;
+
+    Task<bool> SaveChangesAsync();
 }
