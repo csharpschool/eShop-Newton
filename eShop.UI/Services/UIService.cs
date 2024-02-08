50c5a5a098b8a5eeb824171ebfee92973845ea42
Jonas Fagerberg <info@csharpschool.com>
2024-02-08 12:02:30 +0100

    Injecerade UIService/HttpClient

--- eShop.UI/Services/UIService.cs
+++ eShop.UI/Services/UIService.cs
@@ -1,6 +1,18 @@
-﻿
-namespace eShop.UI.Services;
+﻿namespace eShop.UI.Services;
 
 public class UIService(CategoryHttpClient categoryHttp)
 {
+    List<CategoryGetDTO> Categories { get; set; } = [];
+    public List<LinkGroup> CaregoryLinkGroups { get; private set; } =
+    [
+        new LinkGroup { 
+            Name = "Categories",
+            LinkOptions = new(){
+                new LinkOption { Id = 1, Name = "Women", IsSelected = true },
+                new LinkOption { Id = 2, Name = "Men", IsSelected = false },
+                new LinkOption { Id = 3, Name = "Children", IsSelected = false }
+            }
+        }
+    ];
+    public int CurrentCategoryId { get; set; }
 }
